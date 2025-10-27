42a63872d13d42cdb7fd75012132ebe28b829808
jjuanmanu15 <jjuanmanu15@gmail.com>
2025-10-27 15:58:41 -0500

    Agrego script info.php con información dinámica en PHP

--- www/info.php
+++ www/info.php
@@ -1,4 +1,6 @@
 <?php
-  echo "<h1>Hola desde PHP</h1>";
-  phpinfo();
+echo "<h1>Información del servidor PHP</h1>";
+echo "<p>Fecha y hora actual: " . date('Y-m-d H:i:s') . "</p>";
+echo "<p>Servidor: " . gethostname() . "</p>";
+phpinfo();
 ?>
